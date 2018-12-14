f0e96b8e891026ed37d5e10ba63ff595ca3e3961
babe124578 <natthapong41@gmail.com>
2018-12-14 18:25:10 +0700

    add toggle on/off button

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,13 @@ function updateClock() {
     
     setTimeout(updateClock, 1000);
 }
-
+function checkStatus(checkbox){
+    if (checkbox.checked){
+		document.getElementById("toggleStatus").innerHTML = "ON";
+    }else{
+		document.getElementById("toggleStatus").innerHTML = "OFF";
+	}
+}
 $(document).ready(function () {
     updateClock();
 });
