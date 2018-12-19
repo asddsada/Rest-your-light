af9f53cd5e959d5ced7e9498daf8c053785e2754
asddsada <baipo_p@hotmail.com>
2018-12-19 22:34:20 +0700

    Merge hardware

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -1,150 +1,115 @@
-//init default value of all sessionStorage
-if(sessionStorage.getItem("switchModeOnFlag") == null) {
-      sessionStorage.setItem("switchModeOnFlag", false);
-} 
-if(sessionStorage.getItem("switchAutoOnFlag") == null) {
-      sessionStorage.setItem("switchAutoOnFlag", false);
-}
-if(sessionStorage.getItem("switchBrightnessOnFlag") == null) {
-      sessionStorage.setItem("switchBrightnessOnFlag", false);
-}
-//if(sessionStorage.getItem("starttimeFlag") == null) {
-//      sessionStorage.setItem("starttimeFlag", "00:00");
-//}
-//if(sessionStorage.getItem("endtimeFlag") == null) {
-//      sessionStorage.setItem("endtimeFlag", "00:00");
-//}
-//end of init
-
-$(document).ready(function () {
-    updateCheckBox();
-    updateModeCheckBox();
-    updateAutoCheckBox();
-    updateBrightnessCheckBox();
-    //updateStartTime();
-    //updateEndTime();
-});
-
 function checkModeStatus(checkbox) {
+    for (i = 0; i < 2000; i++);
     sessionStorage.setItem('switchModeOnFlag', checkbox.checked);
     console.log(sessionStorage.getItem('switchModeOnFlag'));
+    makeDisabled();
     if (sessionStorage.getItem('switchModeOnFlag') == "true") {
-        console.log('nightowl');
-        document.getElementById("toggleModeStatus").innerHTML = "NightOWL";
+        microgear.chat(thing1, "/21");
+        setTimeout(function () {
+            console.log('nightowl');
+            document.getElementById("toggleModeStatus").innerHTML = "NightOWL";
+            setTimeout(function () {
+                removeDisabled();
+            }, 5000);
+        }, 2000);
     } else {
-        console.log('earlybird');
-        document.getElementById("toggleModeStatus").innerHTML = "EarlyBIRD";
+        microgear.chat(thing1, "/20");
+        setTimeout(function () {
+            console.log('earlybird');
+            document.getElementById("toggleModeStatus").innerHTML = "EarlyBIRD";
+            setTimeout(function () {
+                removeDisabled();
+            }, 5000);
+        }, 2000);
     }
+
 }
 function updateModeCheckBox() {
     console.log(sessionStorage.getItem('switchModeOnFlag'));
+    makeDisabled();
     if (sessionStorage.getItem('switchModeOnFlag') == "true") {
+        microgear.chat(thing1, "/21");
         $('input[type="checkbox"][name*="Mode"]').prop('checked', true);
         document.getElementById("toggleModeStatus").innerHTML = "NightOWL";
     } else {
+        microgear.chat(thing1, "/20");
         $('input[type="checkbox"][name*="Mode"]').prop('checked', false);
         document.getElementById("toggleModeStatus").innerHTML = "EarlyBIRD";
     }
 }
 function checkAutoStatus(checkbox) {
+    for (i = 0; i < 2000; i++);
     sessionStorage.setItem('switchAutoOnFlag', checkbox.checked);
     console.log(sessionStorage.getItem('switchAutoOnFlag'));
+    makeDisabled();
     if (sessionStorage.getItem('switchAutoOnFlag') == "true") {
-        console.log('autoOn');
-        document.getElementById("toggleAutoStatus").innerHTML = "ON";
-		document.getElementById("setBrightness").disabled = true;
-		document.getElementById("lock").innerHTML = '<i class="fas fa-lock"></i>';
+        microgear.chat(thing1, "/31");
+        setTimeout(function () {
+            console.log('autoOn');
+            document.getElementById("toggleAutoStatus").innerHTML = "ON";
+            setTimeout(function () {
+                removeDisabled();
+            }, 5000);
+        }, 2000);
     } else {
-        console.log('autoOff');
-        document.getElementById("toggleAutoStatus").innerHTML = "OFF";
-		document.getElementById("setBrightness").disabled = false;
-		document.getElementById("lock").innerHTML = '';
+        microgear.chat(thing1, "/30");
+        setTimeout(function () {
+            console.log('autoOff');
+            document.getElementById("toggleAutoStatus").innerHTML = "OFF";
+            setTimeout(function () {
+                removeDisabled();
+            }, 5000);
+        }, 2000);
     }
 }
 function updateAutoCheckBox() {
     console.log(sessionStorage.getItem('switchAutoOnFlag'));
+    makeDisabled();
     if (sessionStorage.getItem('switchAutoOnFlag') == "true") {
+        microgear.chat(thing1, "/31");
         $('input[type="checkbox"][name*="Auto"]').prop('checked', true);
         document.getElementById("toggleAutoStatus").innerHTML = "ON";
     } else {
+        microgear.chat(thing1, "/30");
         $('input[type="checkbox"][name*="Auto"]').prop('checked', false);
         document.getElementById("toggleAutoStatus").innerHTML = "OFF";
     }
 }
 function checkBrightnessStatus(checkbox) {
+    for (i = 0; i < 2000; i++);
     sessionStorage.setItem('switchBrightnessOnFlag', checkbox.checked);
     console.log(sessionStorage.getItem('switchBrightnessOnFlag'));
+    makeDisabled();
     if (sessionStorage.getItem('switchBrightnessOnFlag') == "true") {
-        console.log('bright');
-        document.getElementById("toggleBrightnessStatus").innerHTML = "BRIGHT";
+        microgear.chat(thing1, "/41");
+        setTimeout(function () {
+            console.log('bright');
+            document.getElementById("toggleBrightnessStatus").innerHTML = "BRIGHT";
+            setTimeout(function () {
+                removeDisabled();
+            }, 5000);
+        }, 2000);
     } else {
-        console.log('dim');
-        document.getElementById("toggleBrightnessStatus").innerHTML = "DIM";
+        microgear.chat(thing1, "/40");
+        setTimeout(function () {
+            console.log('dim');
+            document.getElementById("toggleBrightnessStatus").innerHTML = "DIM";
+            setTimeout(function () {
+                removeDisabled();
+            }, 5000);
+        }, 2000);
     }
 }
 function updateBrightnessCheckBox() {
     console.log(sessionStorage.getItem('switchBrightnessOnFlag'));
+    makeDisabled();
     if (sessionStorage.getItem('switchBrightnessOnFlag') == "true") {
+        microgear.chat(thing1, "/41");
         $('input[type="checkbox"][name*="Brightness"]').prop('checked', true);
         document.getElementById("toggleBrightnessStatus").innerHTML = "BRIGHT";
     } else {
+        microgear.chat(thing1, "/40");
         $('input[type="checkbox"][name*="Brightness"]').prop('checked', false);
         document.getElementById("toggleBrightnessStatus").innerHTML = "DIM";
     }
 }
-
-/*
-var startauto = $('#starttime');
-startauto.clockpicker({
-    placement: 'top',
-    align: 'left',
-    donetext: 'Done',
-    afterDone: function () {
-        sessionStorage.setItem('starttimeFlag', document.getElementById("starttime").value);
-        console.log(sessionStorage.getItem('starttimeFlag'));
-    }
-});
-
-var endauto = $('#endtime');
-endauto.clockpicker({
-    placement: 'top',
-    align: 'left',
-    donetext: 'Done',
-    afterDone: function () {
-        sessionStorage.setItem('endtimeFlag', document.getElementById("endtime").value);
-        console.log(sessionStorage.getItem('endtimeFlag'));
-    }
-});
-
-// Manual operations
-$('#starttimebtn').click(function (e) {
-    // Have to stop propagation here
-    e.stopPropagation();
-    startauto.clockpicker('show');
-});
-$('#endtimebtn').click(function (e) {
-    // Have to stop propagation here
-    e.stopPropagation();
-    endauto.clockpicker('show');
-});
-
-function updateStartTime() {
-    console.log(sessionStorage.getItem('starttimeFlag'));
-	$('input[type="text"][name*="inpStart"]').prop(document.getElementById("starttime").value);
-	document.getElementById("starttime").value = sessionStorage.getItem('starttimeFlag');
-}
-function updateEndTime() {
-    console.log(sessionStorage.getItem('endtimeFlag'));
-	$('input[type="text"][name*="inpEnd"]').prop(document.getElementById("endtime").value);
-	document.getElementById("endtime").value = sessionStorage.getItem('endtimeFlag');
-}
-
-function clearTime(){
-	sessionStorage.setItem("starttimeFlag", "00:00");
-	sessionStorage.setItem("endtimeFlag", "00:00");
-	document.getElementById("starttime").value = sessionStorage.getItem('starttimeFlag');
-	document.getElementById("endtime").value = sessionStorage.getItem('endtimeFlag');
-	console.log(sessionStorage.getItem('starttimeFlag'));
-	console.log(sessionStorage.getItem('endtimeFlag'));
-}
-*/
